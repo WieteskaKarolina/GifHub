0f753c18a6bf1a38a0aca7e79da301c06a488e1f
WieteskaKarolina <wieteskakarolina02@gmail.com>
2023-06-11 08:26:34 +0200

    add user endpoint and like button

--- app/app.js
+++ app/app.js
@@ -46,10 +46,10 @@ const checkAuthentication = (req, res, next) => {
 
 app.get('/', checkAuthentication, (req, res) => {
     const page = req.query.page || 1;
-    const limit = req.query.limit || 25;
+    const limit = req.query.limit || 36;
     const offset = (page - 1) * limit;
   
-    fetch(`https://api.giphy.com/v1/gifs/search?api_key=2HwtozSNXN1n7iOTOxjiOPC7drs5HadF&q=tiger&limit=${limit}&offset=${offset}&rating=g&lang=en`)
+    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=2HwtozSNXN1n7iOTOxjiOPC7drs5HadF&limit=${limit}&offset=${offset}&rating=g&lang=en`)
       .then(response => response.json())
       .then(data => {
         const imageUrls = [];
@@ -74,6 +74,41 @@ app.post('/favorite', (req, res) => {
 
   res.json({ message: 'Gif dodany do ulubionych' });
 });
+
+
+app.get('/user', checkAuthentication, (req, res) => {
+  const userId = req.session.user.user_id;
+
+  // db.any('SELECT favorites.favorite_id, gifs.gif_url FROM favorites INNER JOIN gifs ON favorites.gif_id = gifs.gif_id WHERE favorites.user_id = $1', [userId])
+  //   .then(favorites => {
+  //     res.render('user', { favorites, username: req.session.user.username });
+  //   })
+  //   .catch(error => {
+  //     console.log('Error:', error);
+  //     res.status(500).send('Internal Server Error');
+  //   });
+
+  const limit = 12;
+  const offset = 0;
+
+  fetch(`https://api.giphy.com/v1/gifs/trending?api_key=2HwtozSNXN1n7iOTOxjiOPC7drs5HadF&limit=${limit}&offset=${offset}&rating=g&lang=en`)
+    .then(response => response.json())
+    .then(data => {
+      const imageUrls = [];
+
+      data.data.forEach(gif => {
+        imageUrls.push(gif.images.fixed_height.url);
+      });
+      res.render('user', { imageUrls , username: req.session.user.username});
+    })
+    .catch(error => {
+      console.log('Error:', error);
+      res.status(500).send('Internal Server Error');
+    });
+
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
